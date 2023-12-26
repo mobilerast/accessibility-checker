28b23f8ec664b29992a8c2f3a831537bb9b06f99
malp <mehmet.alp@rastmobile.com>
2023-12-26 12:18:56 +0300

    feat: accessibilityCheck, package contents

---
+++ accessibilityCheck.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const { AxePuppeteer } = require('axe-puppeteer');
+
+async function runAccessibilityCheck(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setBypassCSP(true);
+
+    await page.goto(url);
+
+    const results = await new AxePuppeteer(page).analyze();
+
+    console.log('Accessibility Violations:');
+    if (results.violations.length === 0) {
+        console.log('No violations found!');
+    } else {
+        results.violations.forEach(violation => {
+            console.log(`${violation.id}: ${violation.description}`);
+            violation.nodes.forEach(node => {
+                console.log(`  ${node.html}`);
+            });
+        });
+    }
+
+    await browser.close();
+}
+
+// Replace with the URL you want to test
+runAccessibilityCheck('https://example.com');
