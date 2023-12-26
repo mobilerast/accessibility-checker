52348523f494933fafa8aacbcce25d1ff1253910
malp <mehmet.alp@rastmobile.com>
2023-12-26 12:32:32 +0300

    feat: axe-puppeteer

--- accessibilityCheck.js
+++ accessibilityCheck.js
@@ -26,4 +26,4 @@ async function runAccessibilityCheck(url) {
 }
 
 // Replace with the URL you want to test
-runAccessibilityCheck('https://example.com');
+runAccessibilityCheck('https://rastmobile.com');
